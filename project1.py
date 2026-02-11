021369158028030ef7ced3c4b2a39d2df0ea5a78
trisha232024F <trisha23@vt.edu>
2026-02-11 13:34:11 -0500

    Refactored division algorithm with lists

--- project1.py
+++ project1.py
@@ -2,39 +2,32 @@
 # Trisha Bajpai, Tommy Dalessio, Kathleen Reece, Theo Tran
 
 def division_algorithm(a: int, b: int):
-    # Defining starting values of the u, v, and q variables
-    # Probably a more elegant way to do this - maybe a list of u variables and a list of v variables?
-    u1 = 1
-    v1 = 0
-    u2 = 0
-    v2 = 1
-    u3 = a
-    v3 = b
+    # Defining starting values of the u, v, and q with lists
+    u = [1, 0, a]
+    v = [0, 1, b]
     q = 0
     # We'll need to store the old values of the u variables to update the v variables
-    old_u1 = 1
-    old_u2 = 2
-    old_u3 = 3
+    old_u = [1, 2, 3]
 
     # Print statements would be replaced by the file print outs in the final program
     print("For a =", a, " and b = ", b)
-    while (v3 > 0):
-        print(u1, v1, u2, v2, u3, v3, q)
-        q = u3 // v3
-        old_u1 = u1
-        old_u2 = u2
-        old_u3 = u3
-        u1 = v1
-        u2 = v2
-        u3 = v3
-        v1 = old_u1 - (q * v1)
-        v2 = old_u2 - (q * v2)
-        v3 = old_u3 - (q * v3)
+    while (v[2] > 0):
+        print(u[0], v[0], u[1], v[1], u[2], v[2], q)
+        q = u[2] // v[2]
+        old_u[0] = u[0]
+        old_u[1] = u[1]
+        old_u[2] = u[2]
+        u[0] = v[0]
+        u[1] = v[1]
+        u[2] = v[2]
+        v[0] = old_u[0] - (q * v[0])
+        v[1] = old_u[1] - (q * v[1])
+        v[2] = old_u[2] - (q * v[2])
 
-    print(u1, v1, u2, v2, u3, v3, q)
-    print("x = ", u1)
-    print("y = ", u2)
-    print("gcd = ", u3)
+    print(u[0], v[0], u[1], v[1], u[2], v[2], q)
+    print("x = ", u[0])
+    print("y = ", u[1])
+    print("gcd = ", u[2])
     print()
 
 def main():
