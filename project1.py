75933a4fc5721f98b8904ba6c78c73f9ea3ff941
Tommy D <tommy1344@vt.edu>
2026-02-13 17:37:19 -0500

    Formatting fixes and general improvements

--- project1.py
+++ project1.py
@@ -1,11 +1,10 @@
 #python3 -m pip install tabulate
 from tabulate import tabulate
 
-# Participating students: 
+# Participating students:
 # Trisha Bajpai, Tommy Dalessio, Kathleen Reece, Theo Tran
 
 def division_algorithm(a: int, b: int):
-   
     # Defining starting values of the u, v, and q with lists
     u = [1, 0, a]
     v = [0, 1, b]
@@ -15,8 +14,8 @@ def division_algorithm(a: int, b: int):
 
     table = []
 
+    # Main algorithm loop. Runs until v2 <= 0.
     while (v[2] > 0):
-        
         table.append([u[0], v[0], u[1], v[1], u[2], v[2], q])
         q = u[2] // v[2]
         
@@ -25,24 +24,28 @@ def division_algorithm(a: int, b: int):
             u[i] = v[i]
             v[i] = old_u[i] - (q * v[i])
 
-    
-
     table.append([u[0], v[0], u[1], v[1], u[2], v[2], q])
 
     #Open textfile and write table information to it
     with open("DivisionOutput.txt", "a") as textFile:
-        textFile.write(f"For a = {a} and b = {b} \n\n")
+        textFile.write(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+        
+        textFile.write(f"For a = {a} and b = {b}\n\n")
 
         textFile.write(
-            tabulate(table, headers=["u1","v1","u2","v2","u3","v3","q"]))   
+            tabulate(table, headers=["u1","v1","u2","v2","u3","v3","q"]))
             
-        textFile.write(f"\n\nx = {u[0]} \n")
+        textFile.write(f"\n\nx = {u[0]}\n")
 
-        textFile.write(f"y =  {u[1]} \n")
+        textFile.write(f"y =  {u[1]}\n")
 
-        textFile.write(f"gcd = {u[2]} \n\n")
+        textFile.write(f"gcd = {u[2]}\n")
 
 def main():
+    # Adding our names to top of file for credit
+    with open("DivisionOutput.txt", "a") as textFile:
+        textFile.write(f"Project 1: Cryptography MATH 4175\n")
+        textFile.write(f"Trisha Bajpai, Tommy Dalessio, Kathleen Reece, Theo Tran\n")
     division_algorithm(768336, 78192)
     division_algorithm(494752, 296864)
     division_algorithm(17601969, 2364768)
